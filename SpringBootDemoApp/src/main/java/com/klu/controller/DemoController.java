3de84f2ea5d0cb210a7f4ac40bcc1982cf9bc9ef
Srinadh <2400032512@kluniversity.in>
2026-01-28 10:45:35 +0530

    Added SpringBootDemoApp project

---
+++ SpringBootDemoApp/src/main/java/com/klu/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.klu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "String Boot Demo A pplication";
+		
+		
+	}
+
+}
